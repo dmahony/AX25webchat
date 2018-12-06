849acaf166b62abd3276cd709095376582495c06
Daniel Mahony <danielwmahony@gmail.com>
2018-12-07 01:47:20 +1100

    Update script.php

--- script.php
+++ script.php
@@ -4,7 +4,6 @@ $field2 = $_POST["edate"];
 
 echo ("$field2");
 
-$output = shell_exec('/home/pi/ax25-packet-network/supervisory/super2 -s ax0 ALLALL '.escapeshellarg($_POST['edate']));
-
+$output = shell_exec('beacon -s ax0 ALLALL '.escapeshellarg($_POST['edate']));
 header('Location: http://0.0.0.0/input.html');
 ?>
