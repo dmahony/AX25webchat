87cf56e85a217b674782408bcfbda92a447010a1
Daniel Mahony <danielwmahony@gmail.com>
2018-12-07 01:36:10 +1100

    Update script.php

--- script.php
+++ script.php
@@ -6,5 +6,5 @@ echo ("$field2");
 
 $output = shell_exec('/home/pi/ax25-packet-network/supervisory/super2 -s ax0 ALLALL '.escapeshellarg($_POST['edate']));
 
-header('Location: http://0.0.0.0/1input.html');
+header('Location: http://0.0.0.0/input.html');
 ?>
